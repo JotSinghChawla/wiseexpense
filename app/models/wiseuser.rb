e0f6497c73d133f25f4cad32f0aad0b035a76591
Jot Singh Chawla <jot.chawla.54.js@gmail.com>
2023-12-15 12:32:58 +0530

    setup wiseuser

---
+++ app/models/wiseuser.rb
@@ -0,0 +1,28 @@
+class Wiseuser < ApplicationRecord
+    
+    has_secure_password
+
+    has_many :expense
+
+    validates :first_name, :last_name, :email, :uuid, presence: true
+    validates :uuid, :email, uniqueness: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+
+    before_validation :set_new_uuid, on: :create
+
+    private
+
+    def set_defaults
+        self.remember_token ||= SecureRandom.urlsafe_base64
+    end
+
+    def set_new_uuid
+        # do while (until unique uuid is not found)
+        begin
+            new_uuid = SecureRandom.uuid
+        end while Wiseuser.find_by_uuid(new_uuid).present?
+
+        self.uuid = new_uuid
+    end
+
+end
