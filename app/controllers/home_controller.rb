3e9e6229cdf672f5c9e29ab3579b2691bbc01484
Jot Singh Chawla <jot.chawla.54.js@gmail.com>
2023-12-16 19:43:25 +0530

    UI for new Expense

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
+  before_action :set_all_expense
+
   def index
+  end
+
+  private
+
+  def set_all_expense
     @all_expense = @current_wiseuser.try(:expenses)
   end
+  
 end
